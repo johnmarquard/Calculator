b08e735a1eb00b4a9ced623c3083f7627f065e8a
johnmarquard <jomszockt@gmx.de>
2018-01-16 17:16:37 +0100

    add code for calc

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wastun = null;
+
+            Console.WriteLine("Wilkommen!");
+            Console.WriteLine("Bitte wählen Sie eine Rechenart: (a)ddieren (s)ubstrahieren");
+            wastun = Console.ReadLine();
+
+            if (wastun == "a")
+            {
+                Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+                int zahl1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+                int zahl2 = Convert.ToInt32(Console.ReadLine());
+                int ergebnis = zahl1 + zahl2;
+                var fertig = $"{zahl1}+{zahl2}={ergebnis}";
+                Console.WriteLine(fertig);
+            }
+            else
+
+            {
+                Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+                int zahl3 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+                int zahl4 = Convert.ToInt32(Console.ReadLine());
+                int ergebnis1 = zahl3 - zahl4;
+                var fertig2 = $"{zahl3}-{zahl4}={ergebnis1}";
+                Console.WriteLine(fertig2);
+
+
+                Console.ReadKey();
+            }
+            }
+        }
+    } 
+            
+
+
+        
+
+    
+
+
