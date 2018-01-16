ce8fefbd3890a40dbe23a61ec57aafb31c83da0b
johnmarquard <jomszockt@gmx.de>
2018-01-16 16:05:49 +0100

    add the first script

---
+++ Calculator1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Willkommen!");
+            Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(zahl1+zahl2);
+
+
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}
