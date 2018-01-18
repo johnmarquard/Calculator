f2a0e960e40b40c7a27f12e1c90feb7bb2486b50
johnmarquard <jomszockt@gmx.de>
2018-01-18 12:57:00 +0100

    delete trash and cleanup code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,152 +8,91 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             //Strings
-                string str = Path.GetRandomFileName();
-                str = str.Replace(".", "");
-            string strraw = Convert.ToString(str);
-            string key = null;
             string isad = null;
             string wastun = null;
             string username = Environment.UserName;
-            string pathkey = $"C:/Users/{username}/Desktop/key4calculator.txt";
-            Console.ForegroundColor = ConsoleColor.DarkGreen; 
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine($"Wilkommen {username}!");
-            Console.WriteLine("Sollen Addons Aktiviert werden y/n?");
-
-
-            isad = Console.ReadLine();
-            if (isad == "y")
-            { 
-                System.IO.File.WriteAllText(pathkey, str);
-            Console.WriteLine("Bitte geben sie den Aktivierungs-Key ein");
-            key = Console.ReadLine();
-                if (key == strraw)
-                {
-                    Console.Clear();
-
-
-                    Console.WriteLine("Bitte wählen Sie aus was sie machen möchten: (a)ddieren (s)ubstrahieren (m)ultiplizieren (d)ividieren (i)nfo (q)uit");
-                    wastun = Console.ReadLine();
-
-                    if (wastun == "a")
-                    {
-                        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
-                        int zahl1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
-                        int zahl2 = Convert.ToInt32(Console.ReadLine());
-                        int ergebnis = zahl1 + zahl2;
-                        var fertig = $"{zahl1}+{zahl2}={ergebnis}";
-                        Console.WriteLine(fertig);
-                        Console.ReadKey();
-                        return;
-                    }
-                    if (wastun == "s")
-
-                    {
-                        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
-                        int zahl3 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
-                        int zahl4 = Convert.ToInt32(Console.ReadLine());
-                        int ergebnis1 = zahl3 - zahl4;
-                        var fertig2 = $"{zahl3}-{zahl4}={ergebnis1}";
-                        Console.WriteLine(fertig2);
-                        Console.ReadKey();
-                        return;
-                    }
-                    if (wastun == "m")
-
-                    {
-                        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
-                        int zahl3 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
-                        int zahl4 = Convert.ToInt32(Console.ReadLine());
-                        int ergebnis1 = zahl3 * zahl4;
-                        var fertig2 = $"{zahl3}*{zahl4}={ergebnis1}";
-                        Console.WriteLine(fertig2);
-                        Console.ReadKey();
-                        return;
-                    }
-                    if (wastun == "d")
-                    {
-                        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
-                        int zahl3 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
-                        int zahl4 = Convert.ToInt32(Console.ReadLine());
-                        int ergebnis1 = zahl3 / zahl4;
-                        var fertig2 = $"{zahl3}/{zahl4}={ergebnis1}";
-                        Console.WriteLine(fertig2);
-                        Console.ReadKey();
-                        return;
-                    }
-                    if (wastun == "i")
-                    {
-                        Console.WriteLine("Dieser Taschenrechner kann Rechnen");
-                        System.Threading.Thread.Sleep(2000);
-                        return;
-
-                    }
-
-                    if (wastun == "q")
-                    {
-                        Console.WriteLine($"Goodbye,{username}!");
-                        System.Threading.Thread.Sleep(2000);
-                        return;
-                    }
-
-                    else
-                    {
-
-                        Console.Clear();
-                    }
-                }
-            }
-            if (isad == "n")
-                
+            Console.WriteLine("Bitte wählen Sie aus was sie machen möchten: (a)ddieren (s)ubstrahieren (m)ultiplizieren (d)ividieren (q)uit");
+            wastun = Console.ReadLine();
+            if (wastun == "a")
             {
-                Console.WriteLine("Bitte wählen Sie eine Rechenart: (a)ddieren (s)ubstrahieren (q)uit");
-                wastun = Console.ReadLine();
-
-                if (wastun == "a")
-                {
-                    Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
-                    int zahl1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
-                    int zahl2 = Convert.ToInt32(Console.ReadLine());
-                    int ergebnis = zahl1 + zahl2;
-                    var fertig = $"{zahl1}+{zahl2}={ergebnis}";
-                    Console.WriteLine(fertig);
-                    Console.ReadKey();
-                    return;
-                }
-                if (wastun == "s")
-
-                {
-                    Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
-                    int zahl3 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
-                    int zahl4 = Convert.ToInt32(Console.ReadLine());
-                    int ergebnis1 = zahl3 - zahl4;
-                    var fertig2 = $"{zahl3}-{zahl4}={ergebnis1}";
-                    Console.WriteLine(fertig2);
-                    Console.ReadKey();
-                    return;
-                }
-                
-                }
-
-                if (wastun == "q")
-            {   Console.WriteLine($"Goodbye, {username}!");
+                FuehreAdditionAus();
+                return;
+            }
+            if (wastun == "s")
+            {
+                FuerhreSubtracktionAus();
+                return;
+            }
+            if (wastun == "m")
+            {
+                FuerhreMultiAus();
+                return;
+            }
+            if (wastun == "d")
+            {
+                FuerhreDivisionAus();
+                return;
+            }
+            if (wastun == "q")
+            {
+                Console.WriteLine($"Goodbye,{username}!");
                 System.Threading.Thread.Sleep(2000);
                 return;
             }
-
             else
-                {
-                    Console.WriteLine("Dies ist keine gültige Eingabe");
-                Console.WriteLine($"Goodbye, {username}!");
-                System.Threading.Thread.Sleep(2000);
+            {
+                Console.WriteLine("Dies ist keine Gültige Eingabe");
+                System.Threading.Thread.Sleep(500);
                 return;
             }
-            }
-        }
+        } 
+    private static void FuehreAdditionAus()
+    {
+        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+        int zahl1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+        int zahl2 = Convert.ToInt32(Console.ReadLine());
+        int ergebnis = zahl1 + zahl2;
+        var fertig = $"{zahl1}+{zahl2}={ergebnis}";
+        Console.WriteLine(fertig);
+        Console.ReadKey();
     }
+    private static void FuerhreSubtracktionAus()
+    {
+        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+        int zahl3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+        int zahl4 = Convert.ToInt32(Console.ReadLine());
+        int ergebnis1 = zahl3 - zahl4;
+        var fertig2 = $"{zahl3}-{zahl4}={ergebnis1}";
+        Console.WriteLine(fertig2);
+        Console.ReadKey();
+    }
+    private static void FuerhreMultiAus()
+    {
+        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+        int zahl3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+        int zahl4 = Convert.ToInt32(Console.ReadLine());
+        int ergebnis1 = zahl3 * zahl4;
+        var fertig2 = $"{zahl3}*{zahl4}={ergebnis1}";
+        Console.WriteLine(fertig2);
+        Console.ReadKey();
+    }
+    private static void FuerhreDivisionAus()
+    {
+        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+        int zahl3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+        int zahl4 = Convert.ToInt32(Console.ReadLine());
+        int ergebnis1 = zahl3 / zahl4;
+        var fertig2 = $"{zahl3}/{zahl4}={ergebnis1}";
+        Console.WriteLine(fertig2);
+        Console.ReadKey();
+    }
+}
+
+}
